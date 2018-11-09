dcac4ce17cdc1d45fa50e47ffc5b0d6be4330116
Dat <dat@terralogic.com>
2018-11-09 16:43:38 +0700

    First Commit

---
+++ hello.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+echo "Hello World"
+
+echo $PWD
+echo $HOME
+
+name=dat
+echo $name
---
+++ userinput.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+echo "Enter your name and your age: "
+read nameofyou yourage
+echo "Your name is : $nameofyou"
+echo "Your age is : $yourage"
