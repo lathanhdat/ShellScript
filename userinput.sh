c423d4f7b5faa84b0efb357009951a2eefd477d6
Dat <dat@terralogic.com>
2018-11-09 16:52:07 +0700

    Second lesson

--- userinput.sh
+++ userinput.sh
@@ -4,3 +4,15 @@ echo "Enter your name and your age: "
 read nameofyou yourage
 echo "Your name is : $nameofyou"
 echo "Your age is : $yourage"
+
+
+
+read -p "Your name : " name
+read -sp "Your pass : " pswd
+echo
+echo "Your name : $name"
+echo "Your password: $pswd"
+
+echo "Enter names: "
+read -a names
+echo "Name 0 : ${names[0]}, Name 1: ${names[1]}"
